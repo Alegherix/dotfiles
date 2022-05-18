7d03a3ddcff60118e959f8a34219db11729675ad
Martin <alegherix@gmail.com>
2022-05-18 23:49:56 +0200

    updated git aliases

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,15 @@ end
 
 function fish_greeting
 end
+
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit -v'
+alias gd='git diff'
+alias gco='git checkout '
+alias gr='git reset --soft HEAD~1'
+alias gl='git pull'
+alias gp='git push'
+alias gcob='git checkout -b'
+alias gw 'git add . && git stash && git checkout -b'
