1f14dc92883e716b63bdb7002a560d1847fcc2c3
Martin <alegherix@gmail.com>
2022-05-23 20:39:38 +0200

    added gcloud to fish path

--- fish/config.fish
+++ fish/config.fish
@@ -15,3 +15,9 @@ alias gl='git pull'
 alias gp='git push'
 alias gcob='git checkout -b'
 alias gw 'git add . && git stash && git checkout -b'
+
+# Load fishmarks (http://github.com/techwizrd/fishmarks)
+. $HOME/.fishmarks/marks.fish
+
+fish_add_path -p /etc/profile
+export CLOUDSDK_PYTHON=$(which python3)
